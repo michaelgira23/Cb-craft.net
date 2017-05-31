0cce4ae38c58d4dd30ee800d3074c421152570a3
Michael Gira <michaelgira23@gmail.com>
2017-06-01 00:35:40 +0200

    Fix server initialization, add template assets

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ const Server = require('./lib/server');
 
 main();
 async function main() {
+	let mc;
 	try {
-		const mc = await Server.createServer('vanilla-server', 'vanilla/1.11.2.jar');
+		mc = await Server.createServer('vanilla-server', 'vanilla/1.11.2.jar');
 		console.log('Created server!', mc);
 	} catch (err) {
 		console.log('Create server error!', err);
+		if (err !== 'Server name already exists!') {
+			console.log('Create server error!', err);
+			return;
+		} else {
+			console.log('Server already exists!');
+		}
+
+		mc = new Server('vanilla-server');
 	}
+
+	mc.start();
 }
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,14 +1,98 @@
 const fs = require('fs-extra');
 const path = require('path');
 
+const ScriptServer = require('scriptserver');
+const scriptServerModules = [
+	require('scriptserver-event'),
+	require('scriptserver-command'),
+	require('scriptserver-util'),
+	require('scriptserver-json'),
+	require('scriptserver-essentials'),
+	require('scriptserver-update')
+];
+
 const jarsDir = path.join(__dirname, '..', '..', 'jars');
 const serversDir = path.join(__dirname, '..', '..', 'servers');
+const templateDir = path.join(__dirname, '..', '..', 'server-template');
 
 module.exports = class Server {
 
-	constructor(serverName) {
+	constructor(serverName, options = {}) {
 		this.serverName = serverName;
 		this.directory = path.join(serversDir, serverName);
+		this.minGb = options.minGb || 2;
+		this.maxGb = options.minGb || 4;
+		this.running = false;
+
+		// Set ScriptServer (wrapper for Minecraft server)
+		this.scriptServer = new ScriptServer({
+			serverprocess: {
+				cwd: this.directory
+			},
+			jsonDir: path.join(this.directory, 'json'),
+			command: {
+				prefix: '!'
+			}
+		});
+
+		for (const module of scriptServerModules) {
+			this.scriptServer.use(module);
+		}
+
+		this.scriptServer.on('console', data => {
+			console.log(`[EVENT "CONSOLE"]`, data);
+		});
+
+		this.scriptServer.on('chat', data => {
+			console.log(`[EVENT "CHAT"]`, data);
+		});
+
+		this.scriptServer.on('login', data => {
+			console.log(`[EVENT "LOGIN"]`, data);
+		});
+
+		this.scriptServer.on('logout', data => {
+			console.log(`[EVENT "LOGOUT"]`, data);
+		});
+
+		this.scriptServer.on('achievement', data => {
+			console.log(`[EVENT "ACHIEVEMENT"]`, data);
+		});
+
+		this.scriptServer.on('start', data => {
+			console.log(`[EVENT "STOP"]`, data);
+		});
+
+		this.scriptServer.on('exit', data => {
+			console.log(`[EVENT "EXIT"]`, data);
+		});
+
+		this.scriptServer.on('onstart', data => {
+			console.log(`[EVENT "ONSTART"]`, data);
+		});
+
+		this.scriptServer.on('onstop', data => {
+			console.log(`[EVENT "ONSTOP"]`, data);
+		});
+	}
+
+	start() {
+		if (!this.running) {
+			this.scriptServer.start(path.join(this.directory, 'server.jar'), [`-Xms${this.minGb}G`, `-Xmx${this.maxGb}G`]);
+		}
+		this.running = true;
+	}
+
+	stop() {
+		if (this.running) {
+			this.scriptServer.stop();
+		}
+		this.running = false;
+	}
+
+	restart() {
+		this.stop();
+		this.start();
 	}
 
 	static createServer(name, jar, options = {}) {
@@ -30,8 +114,8 @@ module.exports = class Server {
 					throw 'Server jar does not exist!';
 				}
 			})
-			// Create server folder
-			.then(() => fs.ensureDir(serverDir))
+			// Copy over boilerplate server
+			.then(() => fs.copy(templateDir, serverDir))
 			// Copy over jar
 			.then(() => fs.copy(jarPath, path.join(serverDir, 'server.jar')))
 			// Return server object
